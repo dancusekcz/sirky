16e412f81695bd4ce5e44d43297f003351746324
dancusekcz <119076615+dancusekcz@users.noreply.github.com>
2023-05-29 11:08:01 +0200

    main (5).cpp

--- main (5).cpp
+++ main (5).cpp
@@ -4,30 +4,47 @@ using namespace std;
 
 int main()
 {
-    int sirky = 24;
+    int sirky = 0;
     int delsirky = 0;
     int hrac1 = 0;
     bool hra = true;
     
-    cout<<"Hra se sirkami, na stole je 24 sirek, napis kolik chces odecist";
+    cout<<"Hra se sirkami, kolik sirek bude na stole?";
+    cin>>sirky;
+    if (sirky > 1 && sirky < 5){
+        cout<<"Dobre, na stole budou "<<sirky<<" sirky";
+    }
+    else{
+        cout<<"Dobre, na stole bude "<<sirky<<" sirek";
+    }
     
     while(hra = true){    
         if(hrac1 < 1) {
             cout<<"\nHraje hrac 1";
             cin>>delsirky;
-            sirky = sirky - delsirky;
-            hrac1 = hrac1 + 1;
-            cout<<"Na stole zbyva "<<sirky<<" sirek"<<endl;
-            //cout<<hrac1<<endl;
+            if (delsirky > sirky){
+                cout << "Nemuzes odebrat vic sirek, nez je na stole!";
+            }
+            else{
+                sirky = sirky - delsirky;
+                hrac1 = hrac1 + 1;
+                cout<<"Na stole zbyva "<<sirky<<" sirek"<<endl;
+                //cout<<hrac1<<endl;
+            }
         }
         
         else {
             cout<<"\nHraje hrac 2";
             cin>>delsirky;
-            sirky = sirky - delsirky;
-            hrac1 = hrac1 - 1;
-            cout<<"Na stole zbyva "<<sirky<<" sirek"<<endl;
-            //cout<<hrac1<<endl;
+            if (delsirky > sirky){
+                cout << "Nemuzes odebrat vic sirek, nez je na stole!";
+            }
+            else{
+                sirky = sirky - delsirky;
+                hrac1 = hrac1 - 1;
+                cout<<"Na stole zbyva "<<sirky<<" sirek"<<endl;
+                //cout<<hrac1<<endl;
+            }
         }
         
         if (sirky <= 0){
